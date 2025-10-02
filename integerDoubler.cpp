c00a79bf5e328cf47bfb98be650915b8df6f4176
mrp5623 <michaelrpriore@gmail.com>
2025-10-02 11:23:23 -0400

    question 1.0

---
+++ integerDoubler.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+
+void integerDoubler(std::string inputFile, std::string outputFile){
+    std::ifstream input(inputFile, std::ios::binary);
+    std::vector<int32_t> doubled;
+
+    while (!input.eof()){
+        int32_t n;
+        input.read(reinterpret_cast<char*>(&n), sizeof(n));
+        doubled.push_back(n);
+    }
+
+    input.close();
+
+    std::ofstream output(outputFile, std::ios::binary);
+
+
+    for (int32_t num : doubled){
+        num*=2;
+        output.write(reinterpret_cast<char*>(&num), sizeof(num));
+    }
+
+    
+    
+}
