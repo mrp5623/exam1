32dcb6ffffe9c875b71474f70b0964ce3d8e44ee
mrp5623 <michaelrpriore@gmail.com>
2025-10-02 11:29:27 -0400

    1.1

--- integerDoubler.cpp
+++ integerDoubler.cpp
@@ -7,7 +7,7 @@ void integerDoubler(std::string inputFile, std::string outputFile){
     std::ifstream input(inputFile, std::ios::binary);
     std::vector<int32_t> doubled;
 
-    while (!input.eof()){
+    while (!input.eof()&&!input.fail()){
         int32_t n;
         input.read(reinterpret_cast<char*>(&n), sizeof(n));
         doubled.push_back(n);
